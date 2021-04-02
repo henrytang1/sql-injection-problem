d7f55d62f88f12763434cb68fde231e867ddddd1
Henry Tang <65838477+henrytang1@users.noreply.github.com>
2021-04-02 00:55:13 -0400

    change to sqlite3

--- query.py
+++ query.py
@@ -1,39 +1,51 @@
 import os
 import sys
-import psycopg2.pool
+import psycopg2
+import sqlite3
 from urllib.parse import urlparse
 
+# def getConn():
+#     db_url = urlparse(os.environ.get('DATABASE_URL'))
+
+#     dbconfig = {
+#         'minconn': 1,
+#         'maxconn': 3,
+#         'user': db_url.username,
+#         'password': db_url.password,
+#         'host': db_url.hostname,
+#         'database': db_url.path[1:],
+#     }
+
+#     con = psycopg2.pool.SimpleConnectionPool(**dbconfig)
+
+#     return con
+
 def getConn():
-    db_url = urlparse(os.environ.get('DATABASE_URL'))
-
-    dbconfig = {
-        'minconn': 1,
-        'maxconn': 3,
-        'user': db_url.username,
-        'password': db_url.password,
-        'host': db_url.hostname,
-        'database': db_url.path[1:],
-    }
-
-    con = psycopg2.pool.SimpleConnectionPool(**dbconfig)
-
-    return con
+    db_path = "users.db"
+    if not os.path.exists(db_path):
+        print(f'database {db_path} not found', file=sys.stderr)
+        raise FileNotFoundError(f'database {db_path} not found')
+        # sys.exit(1)
+    conn = sqlite3.connect(db_path)
+    return conn
 
 
 def loginQuery(params):
-    pool = getConn()
-    conn = pool.getconn()
+    conn = getConn()
+    # conn = pool.getconn()
     cur = conn.cursor()
 
     query = '''
         SELECT code
         FROM users
-        WHERE username = %s AND password = %s;
+        WHERE username = ? AND password = ?;
     '''
 
     try:
         cur.execute(query, (params["username"], params["password"]))
-    except psycopg2.Error as e:
+    except sqlite3.Error as e:
+        # except psycopg2.Error as e:
+        print(f'{e}', file=sys.stderr)
         raise e
     
     res = cur.fetchone()
@@ -47,8 +59,8 @@ def loginQuery(params):
 
 
 def existenceQuery(params):
-    pool = getConn()
-    conn = pool.getconn()
+    conn = getConn()
+    # conn = pool.getconn()
     cur = conn.cursor()
 
     query = '''
@@ -58,7 +70,7 @@ def existenceQuery(params):
 
     try:
         cur.execute(query % params["username"])
-    except psycopg2.Error as e:
+    except sqlite3.Error as e:
         print(f'{e}', file=sys.stderr)
         raise e
     
